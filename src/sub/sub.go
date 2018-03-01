e6c3c5f864cc426036db0c4817069f42fa4c9574
MOTEAU Nicolas IMT/OLS <NEUM7413@orange.com>
2018-03-01 19:35:11 +0100

    Add sub ToFile

--- src/sub/sub.go
+++ src/sub/sub.go
@@ -9,13 +9,23 @@ const TAG = "SUB"
 
 
 type Sub struct {
-
+	Sub [][]int // list of list of rides id
 }
 
 func (s *Sub) ToFile(name string) {
 	score := 0
 	writer := fileutil.CreateSubFile(name, score)
 
-	fmt.Fprint(writer, "0")
+	//fmt.Fprint(writer, "0")
+	for i:=0; i<len(s.Sub); i++ {
+		vehicleRides := s.Sub[i]
+		M := len(vehicleRides)
+		fmt.Fprint(writer, M, " ")
+		for j:=0; j<M; j++ {
+			fmt.Fprint(writer, vehicleRides[j], " ")
+		}
+		fmt.Fprintln(writer, "")
+	}
+
 	writer.Flush()
 }
