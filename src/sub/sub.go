db56197ab21640eced8cad0af7117dfca10266a8
MOTEAU Nicolas IMT/OLS <NEUM7413@orange.com>
2018-03-01 21:22:42 +0100

    Fix score in sub file

--- src/sub/sub.go
+++ src/sub/sub.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"pbm"
 	"logger"
-	"os"
 )
 
 const TAG = "SUB"
@@ -15,8 +14,25 @@ type Sub struct {
 	Sub [][]int // list of list of rides id
 }
 
-func (s *Sub) ToFile(name string, p pbm.Pbm) {
+func (s *Sub) Score(p pbm.Pbm) int {
 	score := 0
+	for i:=0; i<len(s.Sub); i++ {
+		vehicleRides := s.Sub[i]
+		M := len(vehicleRides)
+		for j:=0; j<M; j++ {
+			rideId := vehicleRides[j]
+			ride := p.Rides[rideId]
+			score += ride.Dist
+			if ride.StartOnTime {
+				score += p.Bonus
+			}
+		}
+	}
+	return score
+}
+
+func (s *Sub) ToFile(name string, p pbm.Pbm) {
+	score := s.Score(p)
 	writer := fileutil.CreateSubFile(name, score)
 
 	//fmt.Fprint(writer, "0")
@@ -26,17 +42,11 @@ func (s *Sub) ToFile(name string, p pbm.Pbm) {
 		fmt.Fprint(writer, M, " ")
 		for j:=0; j<M; j++ {
 			rideId := vehicleRides[j]
-			ride := p.Rides[rideId]
-			score += ride.Dist
-			if ride.StartOnTime {
-				score += p.Bonus
-			}
 			fmt.Fprint(writer, rideId, " ")
 		}
 		fmt.Fprintln(writer, "")
 	}
 	writer.Flush()
 
-	os.Rename(name+"_0", name+"_"+string(score))
 	logger.D(TAG, "File name=%s : score: %v", name, score)
 }
